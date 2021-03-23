04285414dc5ea7187892fa808fae9451a634e2ff
YanTylets <72673268+YanTylets@users.noreply.github.com>
2021-03-23 16:57:19 +0300

    feat: add new arr

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,20 +1,37 @@
 const CustomError = require("../extensions/custom-error");
-
-module.exports = function transform(arr) {
-   if (!Array.isArray(arr)) throw 'Error';
+module.exports = function transform(arr1) {
+  if (!Array.isArray(arr1)) throw 'Error';
+  let arr = [...arr1];
   for (let i = 0; i < arr.length; i++) {
-      if (typeof(arr[i]) == 'string' ) {
-          if (arr[i] == '--discard-next') {
-              arr.splice([i], 2);
-          } else if (arr[i] == '--discard-prev') {
-              arr.splice([i]-1, 2);
-          } else if (arr[i] == '--double-next') {
-              arr.splice([i], 1, arr[i+1])
-          } else if (arr[i] == '--double-prev') {
-              arr.splice([i], 1, arr[i-1])
-          }
-          i = 0;
-  }
+    if (typeof(arr[i]) == 'string' ) {
+        if (arr[i] == '--discard-next') {
+            if ([i] == (arr.length-1)) {
+                arr.splice([i], 1)
+            } else {
+            arr[i+1] = undefined;
+            arr.splice([i], 1);
+            }
+        } else if (arr[i] == '--discard-prev') {
+            if (i == 0) {
+                arr.splice([i], 1)
+            } else {
+            arr[i-1] = undefined;
+            arr.splice([i], 1);
+            }
+        } else if (arr[i] == '--double-next') {
+            if ([i] == (arr.length-1)) {
+                arr.splice([i], 1)
+            } else {
+            arr.splice([i], 1, arr[i+1])}
+        } else if (arr[i] == '--double-prev') {
+            if (i == 0) {
+                arr.splice([i], 1)
+            } else {
+            arr.splice([i], 1, arr[i-1])
+            }
+        }
+        i = 0;
 }
-  return arr;
-};
+}
+return arr.filter(item => typeof(item) == 'number');
+}

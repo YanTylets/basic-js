d22b52dfcac800f582f77dd104df5125f3f5a1c1
YanTylets <72673268+YanTylets@users.noreply.github.com>
2021-03-23 17:49:33 +0300

    fix: delete ifinity cicle

--- src/transform-array.js
+++ src/transform-array.js
@@ -7,31 +7,38 @@ module.exports = function transform(arr1) {
         if (arr[i] == '--discard-next') {
             if ([i] == (arr.length-1)) {
                 arr.splice([i], 1)
+                i = 0;
             } else {
             arr[i+1] = undefined;
             arr.splice([i], 1);
+            i = 0;
             }
         } else if (arr[i] == '--discard-prev') {
             if (i == 0) {
-                arr.splice([i], 1)
+                arr.splice([i], 1);
+                i = 0;
             } else {
             arr[i-1] = undefined;
             arr.splice([i], 1);
+            i = 0;
             }
         } else if (arr[i] == '--double-next') {
             if ([i] == (arr.length-1)) {
-                arr.splice([i], 1)
+                arr.splice([i], 1);
+                i = 0;
             } else {
             arr.splice([i], 1, arr[i+1])}
+            i = 0;
         } else if (arr[i] == '--double-prev') {
             if (i == 0) {
                 arr.splice([i], 1)
+                i = 0;
             } else {
             arr.splice([i], 1, arr[i-1])
+            i = 0;
             }
         }
-        i = 0;
 }
 }
-return arr.filter(item => typeof(item) == 'number');
+return arr.filter(item => item !== undefined);
 }
